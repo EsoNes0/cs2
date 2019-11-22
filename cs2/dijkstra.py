13dff5009816ac5a1c70267344acc3c0c149fbe2
Larsen Close <esones0@protonmail.com>
2019-11-22 00:02:46 -0700

    dijkstras style fixes

--- cs2/dijkstra.py
+++ cs2/dijkstra.py
@@ -1,23 +1,37 @@
+# -*- coding: utf-8 -*-
+'''Dijkstra's algorithm implementation
 
 
+A recursive method that takes 1) a string to find, 2) a string to replace
+the found string with, and 3) an initial string. Return the initial string with
+all the found strings replaced with the replacement string. Without using loops
+or the built-in string methods except comparison, length, and slicing.
+
+
+Author: Larsen Close
+Version: Completed through A level work
+Outline and initial tests provided in class by Professor Dr. Beaty at MSU Denver
+
+
+Todo:
+    * Make runnable from file
+    * Also also use ``sphinx.ext.todo`` extension
 '''
-    Description: Dijkstra's algorithm implementation
-    Author: Larsen Close
-    Version: Completed through A level work
-    Outline and initial tests provided in class by Professor Dr. Beaty at MSU Denver
-'''
+
+import sys
 
 
 TRACK_PREVIOUS = True
 
 
-class Weighted_Digraph:
-    class __edge(object):
+class WeightedDigraph:
+    '''Weighted digraph class for dijkstra's algorithm implementation'''
+    class _edge():
         def __init__(self, to_node, weight):
             self.to_node = to_node
             self.weight = weight
 
-    class __node(object):
+    class _node():
         def __init__(self, value):
             self.value = value
             self.edges = []
@@ -27,59 +41,67 @@ class Weighted_Digraph:
             for edge in self.edges:
                 resultant += "->" + str(edge.to_node.value) + \
                           "(" + str(edge.weight) + ")"
-            return (resultant)
+            return resultant
 
         def add_edge(self, new_edge):
+            '''add edge method for weighted digraph class'''
             if not self.is_adjacent(new_edge.to_node):
                 self.edges.append(new_edge)
 
         def remove_edge(self, to_node):
+            '''remove edge method for weighted digraph class'''
             for edge in self.edges:
                 if edge.to_node == to_node:
                     self.edges.remove(edge)
 
         def is_adjacent(self, node):
+            '''checks adjanency to single node'''
             for edge in self.edges:
                 if edge.to_node == node:
-                    return (True)
-            return (False)
+                    return True
+            return False
 
     def __init__(self, directed=True):
         self.__nodes = []
         self.__directed = directed
 
     def __len__(self):
-        return (len(self.__nodes))
+        return len(self.__nodes)
 
     def __str__(self):
         resultant = ""
         for node in self.__nodes:
             resultant += str(node) + '\n'
-        return (resultant)
+        return resultant
 
     def __iter__(self):
         return iter(self.__nodes)
 
     def find(self, value):
+        '''find method for weighted digraph class'''
         for node in self:
             if node.value == value:
-                return (node)
+                return node
 
-        return (None)
+        return None
 
     def add_nodes(self, nodes):
+        '''add nodes plural method, calls add node'''
         for node in nodes:
             self.add_node(node)
 
     def add_node(self, value):
+        '''add node method for weighted digraph class'''
         if not self.find(value):
-            self.__nodes.append(self.__node(value))
+            self.__nodes.append(self._node(value))
 
     def add_edges(self, edges):
+        '''add edges plural method, calls add edge'''
         for edge in edges:
             self.add_edge(edge[0], edge[1], edge[2])
 
     def add_edge(self, from_value, to_value, weight):
+        '''add edge method for weighted digraph class'''
         from_node = self.find(from_value)
         to_node = self.find(to_value)
 
@@ -90,11 +112,12 @@ class Weighted_Digraph:
             self.add_node(to_value)
             to_node = self.find(to_value)
 
-        from_node.add_edge(self.__edge(to_node, weight))
+        from_node.add_edge(self._edge(to_node, weight))
         if not self.__directed:
-            to_node.add_edge(self.__edge(from_node, weight))
+            to_node.add_edge(self._edge(from_node, weight))
 
     def remove_edge(self, from_value, to_value, weight):
+        '''remove edge method for weighted digraph class'''
         from_node = self.find(from_value)
         to_node = self.find(to_value)
 
@@ -103,23 +126,27 @@ class Weighted_Digraph:
             to_node.remove_edge(from_node)
 
     def are_adjacent(self, value1, value2):
-        return (self.find(value1).is_adjacent(self.find(value2)))
+        '''method to check if two nodes are adjacent'''
+        return self.find(value1).is_adjacent(self.find(value2))
 
     def infandnone(self):
+        '''changes distance to unbound upper limit and previous to none '''
         for node in self:
             node.distance = float("inf")
             node.previous = None
 
-    def TRACK_PREVIOUS(self, end):
-        path = []
+    def track_previous(self, end):
+        '''method for tracking the previous paths'''
+        _path = []
         last = self.find(end)
         while last.previous is not None:
-            path.append(last.previous.value)
+            _path.append(last.previous.value)
             last = last.previous
 
-        return path
+        return _path
 
     def dijkstra(self, start):
+        '''dijkstras implementation'''
 
         if not self:
             raise RuntimeError("no graph")
@@ -128,7 +155,7 @@ class Weighted_Digraph:
             for node in self:
                 node.distance = 0
                 node.previous = None
-                return
+                return False
 
         self.infandnone()
         source = self.find(start)
@@ -140,7 +167,6 @@ class Weighted_Digraph:
         todo.add(source)
 
         result = []
-        final = []
 
         while todo:
 
@@ -149,30 +175,30 @@ class Weighted_Digraph:
             for node in todo:
 
                 if node.distance < smallest:
-                    min = node
+                    _min = node
                     smallest = node.distance
 
-            todo.remove(min)
-            result.append([min.distance, min.value])
+            todo.remove(_min)
+            result.append([_min.distance, _min.value])
 
-            for edge in min.edges:
-                alt = min.distance + edge.weight
+            for edge in _min.edges:
+                alt = _min.distance + edge.weight
 
                 if alt < edge.to_node.distance:
                     edge.to_node.distance = alt
-                    edge.to_node.previous = min
+                    edge.to_node.previous = _min
 
                     todo.add(edge.to_node)
 
         for item in result:
-            item.extend(self.TRACK_PREVIOUS(item[1]))
+            item.extend(self.track_previous(item[1]))
 
         return result
 
 
 if __name__ == '__main__':
-    GRAPH = Weighted_Digraph(False)
-    FILE = open(argv[1], "r")
+    GRAPH = WeightedDigraph(False)
+    FILE = open(sys.argv[1], "r")
     for line in FILE:
         a = line.strip().split(" ")
         GRAPH.add_edge(a[0], a[1], int(a[2]))
--- tests/test_dijkstra_algorithm.py
+++ tests/test_dijkstra_algorithm.py
@@ -3,41 +3,41 @@
 
 
 import unittest
-from cs2.dijkstra import Weighted_Digraph, TRACK_PREVIOUS
+from cs2.dijkstra import WeightedDigraph, TRACK_PREVIOUS
 # pylint: disable=invalid-name
 # pylint: disable=no-value-for-parameter
 
 
-class test_weighted_digraph(unittest.TestCase):
+class test_WeightedDigraph(unittest.TestCase):
     """Weigted digraph class for dijkstras"""
 
     def test_empty(self):
         """Tests if digraph is empty"""
-        self.assertEqual(len(Weighted_Digraph()), 0)
+        self.assertEqual(len(WeightedDigraph()), 0)
 
     def test_one(self):
         """Tests if digraph has one"""
-        g = Weighted_Digraph()
+        g = WeightedDigraph()
         g.add_node(1)
         self.assertEqual(len(g), 1)
 
     def test_duplicate(self):
         """Tests if digraph has duplicates"""
-        g = Weighted_Digraph()
+        g = WeightedDigraph()
         g.add_node(1)
         g.add_node(1)
         self.assertEqual(len(g), 1)
 
     def test_two(self):
         """Tests if digraph has 2"""
-        g = Weighted_Digraph()
+        g = WeightedDigraph()
         g.add_node(1)
         g.add_node(2)
         self.assertEqual(len(g), 2)
 
     def test_edge(self):
         """Tests digraph edges"""
-        g = Weighted_Digraph()
+        g = WeightedDigraph()
         g.add_node(1)
         g.add_node(2)
         g.add_edge(1, 2, 3)
@@ -45,14 +45,14 @@ class test_weighted_digraph(unittest.TestCase):
 
     def test_adding_ints(self):
         """Tests adding ints"""
-        g = Weighted_Digraph()
+        g = WeightedDigraph()
         g.add_nodes([1, 2])
         g.add_edges([(1, 2, 3), (2, 1, 3)])
         self.assertEqual(str(g), '1->2(3)\n2->1(3)\n')
 
     def test_adding_strings(self):
         """Tests adding strings"""
-        g = Weighted_Digraph()
+        g = WeightedDigraph()
         g.add_nodes(['Denver', 'Boston'])
         g.add_edges([('Denver', 'Boston', 1971.8),
                      ('Boston', 'Denver', 1971.8)])
@@ -61,7 +61,7 @@ class test_weighted_digraph(unittest.TestCase):
 
     def test_are_adjacent(self):
         """Tests adjacency"""
-        g = Weighted_Digraph()
+        g = WeightedDigraph()
         g.add_nodes(['Denver', 'Boston'])
         g.add_edges([('Denver', 'Boston', 1971.8),
                      ('Boston', 'Denver', 1971.8)])
@@ -69,7 +69,7 @@ class test_weighted_digraph(unittest.TestCase):
 
     def test_arent_adjacent(self):
         """Tests non-adjacency"""
-        g = Weighted_Digraph()
+        g = WeightedDigraph()
         g.add_nodes(['Denver', 'Boston', 'Milano'])
         g.add_edges([('Denver', 'Boston', 1971.8),
                      ('Boston', 'Denver', 1971.8)])
@@ -77,7 +77,7 @@ class test_weighted_digraph(unittest.TestCase):
 
     def test_arent_adjacent_directed(self):
         """Tests non-adjacency directed"""
-        g = Weighted_Digraph()
+        g = WeightedDigraph()
         g.add_edges([('Denver', 'Boston', 1971.8)])
         self.assertFalse(g.are_adjacent('Denver', 'Milano'))
         self.assertFalse(g.are_adjacent('Boston', 'Denver'))
@@ -85,14 +85,14 @@ class test_weighted_digraph(unittest.TestCase):
 
     def test_arent_adjacent_undirected(self):
         """Tests non-adjacency non-directed"""
-        g = Weighted_Digraph(False)
+        g = WeightedDigraph(False)
         g.add_edges([('Denver', 'Boston', 1971.8)])
         self.assertTrue(g.are_adjacent('Boston', 'Denver'))
         self.assertTrue(g.are_adjacent('Denver', 'Boston'))
 
     def test_add_edges_without_nodes(self):
         """Tests adges without nodes"""
-        g = Weighted_Digraph()
+        g = WeightedDigraph()
         g.add_edges([('Denver', 'Boston', 1971.8),
                      ('Boston', 'Denver', 1971.8)])
         self.assertEqual(str(g),
@@ -100,18 +100,18 @@ class test_weighted_digraph(unittest.TestCase):
 
     def empty_graph(self):
         """Testing dijkstras"""
-        self.assertRaises(RuntimeError, lambda: Weighted_Digraph.dijkstra(0))
+        self.assertRaises(RuntimeError, lambda: WeightedDigraph.dijkstra(0))
 
     def unfound_start(self):
         """Testing dijkstras"""
-        g = Weighted_Digraph(False)
+        g = WeightedDigraph(False)
         g.add_nodes(["Denver"])
         self.assertRaises(
-            RuntimeError, lambda: Weighted_Digraph.dijkstra(None))
+            RuntimeError, lambda: WeightedDigraph.dijkstra(None))
 
     def test_infandnone(self):
         """Testing dijkstras"""
-        g = Weighted_Digraph(False)
+        g = WeightedDigraph(False)
         g.add_edges([(1, 2, 2), (1, 3, 1), (2, 3, 1), (2, 4, 1),
                      (2, 5, 2), (3, 5, 5), (4, 5, 3), (4, 6, 6), (5, 6, 1)])
         g.infandnone()
@@ -136,7 +136,7 @@ class test_weighted_digraph(unittest.TestCase):
         #    \|      \|/
         #     3---5---5
         # '''
-        g = Weighted_Digraph(False)
+        g = WeightedDigraph(False)
         g.add_edges([(1, 2, 2), (1, 3, 1), (2, 3, 1), (2, 4, 1),
                      (2, 5, 2), (3, 5, 5), (4, 5, 3), (4, 6, 6), (5, 6, 1)])
         if not TRACK_PREVIOUS:
